c5d7ffd10aa00bc1e23a084bce1604541eda71de
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2025-03-01 14:22:20 +0900

    유저 프로필 페이지에 데이터 연결 (#8)

--- app/routes/profile.$userId.tsx
+++ app/routes/profile.$userId.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from '@remix-run/cloudflare';
 
+import { showLoader } from 'app/features/profile/loader';
 import Show from 'app/features/profile/pages/show';
 
 export const meta: MetaFunction = () => {
@@ -7,3 +8,5 @@ export const meta: MetaFunction = () => {
 };
 
 export default Show;
+
+export { showLoader as loader };
