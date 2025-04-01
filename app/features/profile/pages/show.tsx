a93d439884fff2f3d365deaa5b56fa894990427e
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2025-04-01 11:39:19 +0900

    수정 페이지 버튼 추가 (#31)

--- app/features/profile/pages/show.tsx
+++ app/features/profile/pages/show.tsx
@@ -1,17 +1,35 @@
+import { Link } from '@remix-run/react';
+
 import styles from './show.module.scss';
 import SongItem from '../components/SongItem';
 
 export default function Show() {
+  const isCurrentUserProfile = true; // Replace with actual logic to determine if it's the current user's profile
+
   return (
     <>
       <div className={styles.todayRecommendBox}>
-        <p className={styles.title}>👍 오늘의 추천곡</p>
+        <div className={styles.titleBox}>
+          <p className={styles.title}>👍 오늘의 추천곡</p>
+          {isCurrentUserProfile && (
+            <Link className={styles.goToEditLink} to="../editlist">
+              수정하기
+            </Link>
+          )}
+        </div>
         <div className={styles.songBox}>
           <SongItem />
         </div>
       </div>
       <div className={styles.todayRecommendBox}>
-        <p className={styles.title}>👑 노래 랭킹</p>
+        <div className={styles.titleBox}>
+          <p className={styles.title}>👑 노래 랭킹</p>
+          {isCurrentUserProfile && (
+            <Link className={styles.goToEditLink} to="../editlist">
+              수정하기
+            </Link>
+          )}
+        </div>
         <div className={styles.songBox}>
           <SongItem />
           <SongItem />
