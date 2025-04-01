142ed153ab9ca61cbca6027b4eee8772d389bbff
dongcheolpark <head02325@gmail.com>
2025-04-01 11:46:59 +0900

    a

--- app/features/profile/pages/editList.tsx
+++ app/features/profile/pages/editList.tsx
@@ -1,12 +1,19 @@
 import { Link } from '@remix-run/react';
 
+import styles from './editList.module.scss';
 import SongItem from '../components/SongItem';
 
 export default function EditList() {
   return (
     <>
       <Link to="../show">Go Back</Link>
-      <SongItem />
+      <div className={styles.editListContainer}>
+        <SongItem />
+        <SongItem />
+        <SongItem />
+        <SongItem />
+        <SongItem />
+      </div>
     </>
   );
 }
