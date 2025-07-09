86077ca52b151bd05e4209b4c9df0e26135b17d2
mokjakA <youseungjua@gmail.com>
2025-07-10 02:09:34 +0900

    fix: eslint & prettier 적용 (250710)

--- app/features/login/loader.tsx
+++ app/features/login/loader.tsx
@@ -1,12 +1,12 @@
-import createLoader from "app/utils/createLoader";
+import createLoader from 'app/utils/createLoader';
 
 export const loginLoader = createLoader(async () => {
-    const clientId = process.env.GOOGLE_CLIENT_ID;
-    const redirectUri = process.env.GOOGLE_REDIRECT_URI;
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+  const redirectUri = process.env.GOOGLE_REDIRECT_URI;
 
-    if(!clientId || !redirectUri){
-        throw new Error("Missing Google OAuth environment variables.");
-    }
+  if (!clientId || !redirectUri) {
+    throw new Error('Missing Google OAuth environment variables.');
+  }
 
-    return { clientId, redirectUri };
-})
+  return { clientId, redirectUri };
+});
--- app/routes/login._index.tsx
+++ app/routes/login._index.tsx
@@ -4,7 +4,7 @@ import { loginLoader } from 'app/features/login/loader';
 import LoginPage from 'app/features/login/pages';
 
 export const meta: MetaFunction = () => {
-    return [{title: 'Login'}, {name: 'description', content: 'Google 계정으로 로그인'}]
+  return [{ title: 'Login' }, { name: 'description', content: 'Google 계정으로 로그인' }];
 };
 
 export { loginLoader as loader };
