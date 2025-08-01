5342827f6a406ddd375755aa8ae9225486fff936
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2025-08-01 17:10:34 +0900

    페이지 넘어다닐 때 상단 progress bar 표시 (#79)
    NProgress 라이브러리를 사용했습니다.

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,9 @@
 import type { LinksFunction } from '@remix-run/node';
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import { Links, Meta, Outlet, Scripts, ScrollRestoration, useNavigation } from '@remix-run/react';
+import NProgress from 'nprogress';
+import { useEffect } from 'react';
+
+import nProgressStyles from 'nprogress/nprogress.css?url';
 
 import 'styles/main.scss';
 
@@ -18,12 +22,14 @@ export const links: LinksFunction = () => [
     rel: 'icon',
     href: '/favicon.ico',
   },
+  { rel: 'stylesheet', href: nProgressStyles },
 ];
 
 export const meta = () => [
   { title: '두둠 음악' },
   { name: 'description', content: '스쳐 가는 멜로디, 머무는 감정들. 음악을 쉽게 기록해 보세요.' },
 ];
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -34,12 +40,27 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <ProgressBar />
         {children}
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
   );
+}
+
+export function ProgressBar() {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    if (navigation.state === 'loading' || navigation.state === 'submitting') {
+      NProgress.start();
+    } else {
+      NProgress.done();
+    }
+  }, [navigation.state]);
+
+  return null;
 }
 
 export default function App() {
