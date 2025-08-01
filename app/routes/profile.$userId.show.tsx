f8e9d5a01f682b0f9def35ea2012b49292d88b76
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2025-08-01 15:25:37 +0900

    프로필 페이지 레이아웃 디자인 (#73)
    이제 프로필 페이지에 네비게이션을 만들려고 합니다.
    모바일과 데스크탑을 구분해서 다른 네비게이션을 적용시키겠습니다.
    
    본격적인 개발에 앞서서 코드를 정리했습니다.
     - 필요없는 코드 제거
     - Profile 페이지에 있는 코드들 Show 페이지로 이동

--- app/routes/profile.$userId.show.tsx
+++ app/routes/profile.$userId.show.tsx
@@ -1,11 +1,5 @@
-import type { MetaFunction } from '@remix-run/node';
-
 import { profileLoader } from 'app/features/profile/loader';
 import Show from 'app/features/profile/pages/show';
-
-export const meta: MetaFunction = () => {
-  return [{ title: 'Post Index' }];
-};
 
 export default Show;
 
--- app/routes/profile.$userId.tsx
+++ app/routes/profile.$userId.tsx
@@ -1,12 +1,3 @@
-import type { MetaFunction } from '@remix-run/node';
-
-import { profileLoader } from 'app/features/profile/loader';
 import Profile from 'app/features/profile/pages/profile';
 
-export const meta: MetaFunction = () => {
-  return [{ title: 'Post Index' }];
-};
-
 export default Profile;
-
-export { profileLoader as loader };
