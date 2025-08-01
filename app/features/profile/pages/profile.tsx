f029e9f55c310deba420e54dae6c0d365e897730
dongcheolpark <head02325@gmail.com>
2025-08-01 15:39:05 +0900

    fix

--- app/features/profile/pages/profile.tsx
+++ app/features/profile/pages/profile.tsx
@@ -1,20 +1,40 @@
-import { Outlet } from '@remix-run/react';
+import { Link, Outlet } from '@remix-run/react';
 
 import { isMobile } from 'app/utils/responsive';
 
+import styles from './profile.module.scss';
+
 export default function Profile() {
-  return (
-    <div>
-      {isMobile() ? <MobileLayout /> : <DesktopLayout />}
-      <Outlet />
-    </div>
-  );
+  return isMobile() ? <MobileLayout /> : <DesktopLayout />;
 }
 
 export const DesktopLayout = () => {
-  return <div>DesktopLayout</div>;
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 };
 
 export const MobileLayout = () => {
-  return <div>MobileLayout</div>;
+  return (
+    <div className={styles.mobile}>
+      <div className={styles.outlet}>
+        <Outlet />
+      </div>
+      <nav className={styles.navigation}>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/search">Search</Link>
+          </li>
+          <li>
+            <Link to="/profile/redirect">Profile</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
