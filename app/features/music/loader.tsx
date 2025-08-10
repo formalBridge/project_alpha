ef29169ac7f4f1dee7c5b25ce5b850303011e3b3
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2025-08-10 19:15:26 +0900

    음악 메모 실제로 표시 (#118)
    <img width="5088" height="3032" alt="image"
    src="https://github.com/user-attachments/assets/48a86ec9-d9a9-4ce3-b720-cebd7ff7f2bc"
    />

--- app/features/music/loader.tsx
+++ app/features/music/loader.tsx
@@ -1,10 +1,17 @@
+import { getCurrentUser } from 'app/external/auth/jwt.server';
 import createLoader from 'app/utils/createLoader';
 
-export const musicUserLoader = createLoader(async ({ db, params }) => {
+export const musicUserLoader = createLoader(async ({ db, params, request }) => {
   const songId = Number(params.songId);
   const userId = Number(params.userId);
 
+  const currentUser = await getCurrentUser(request);
+  const isCurrentUserProfile = currentUser?.id === userId;
+
   const song = await db.song.findUnique({ where: { id: songId } });
   const user = await db.user.findUnique({ where: { id: userId } });
-  return { song, user };
+
+  const UserMusicMemo = await db.userMusicMemo.findFirst({ where: { userId, songId } });
+
+  return { song, user, UserMusicMemo, isCurrentUserProfile };
 });
