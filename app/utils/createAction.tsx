1f726e0482e9f81f334b7a2eb38817be415b8f1d
dongcheolpark <head02325@gmail.com>
2025-10-08 22:34:22 +0900

    a

--- app/utils/createAction.tsx
+++ app/utils/createAction.tsx
@@ -1,11 +1,12 @@
 import { PrismaClient } from '@prisma/client';
-import { ActionFunctionArgs } from '@remix-run/node';
+import { ActionFunctionArgs } from '@remix-run/server-runtime';
+
+import { prisma } from './getPrisma';
 
 export default function createAction<ActionReturnType>(
   action: (args: ActionFunctionArgs & { db: PrismaClient }) => ActionReturnType
 ) {
   return (args: ActionFunctionArgs) => {
-    const prisma = new PrismaClient();
     return action({ ...args, db: prisma });
   };
 }
--- app/utils/createLoader.tsx
+++ app/utils/createLoader.tsx
@@ -1,11 +1,12 @@
 import { PrismaClient } from '@prisma/client';
 import { LoaderFunctionArgs } from '@remix-run/node';
 
+import { prisma } from './getPrisma';
+
 export default function createLoader<LoaderReturnType>(
   loader: (args: LoaderFunctionArgs & { db: PrismaClient }) => LoaderReturnType
 ) {
   return (args: LoaderFunctionArgs) => {
-    const prisma = new PrismaClient();
     return loader({ ...args, db: prisma });
   };
 }
